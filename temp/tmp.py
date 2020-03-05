4b4d6de15a195b304685e9564ddbf510344f3bc0
Super-Original-Username <aswordlessninja@gmail.com>
2020-03-05 16:12:31 -0700

    updates to tmp

--- temp/tmp.py
+++ temp/tmp.py
@@ -1,7 +1,8 @@
 import threading
 import socket
+import tkinter as tk
 
-ADDR = "10.200.28.219"
+ADDR = "10.200.52.62"
 PORT = 8080
 
 
@@ -10,7 +11,7 @@ class NetThread(threading.Thread):
         threading.Thread.__init__(self)
         self.sock = socket.socket()
         self.sock.connect((ADDR,PORT))
-        self.sock.sendall("the end is nigh\n".encode())
+        #self.sock.sendall("I am alive\n".encode())
 
     def run(self):
         l = Listen(self.sock)
@@ -20,7 +21,7 @@ class NetThread(threading.Thread):
 
 
 
-class Listen(threading.Thread):
+class Sender(threading.Thread):
     def __init__(self,sock):
         threading.Thread.__init__(self)
         self.sock = sock
@@ -33,7 +34,7 @@ class Listen(threading.Thread):
 
 
 
-class Sender(threading.Thread):
+class Listen(threading.Thread):
     def __init__(self,sock):
         threading.Thread.__init__(self)
         self.sock = sock
@@ -41,11 +42,55 @@ class Sender(threading.Thread):
     def run(self):
         prev_msg = ''
         while(True):
-            msg = self.sock.recv(1024)
-            if msg != prev_msg:
-                prev_msg = msg
-                print(msg)
+            data = self.sock.recv(1024).decode()
+            if data != prev_msg:
+                prev_msg = data
+                print(data)
                 #self.sock.close()
+
+
+
+
+
+
+''' TK window thingeroos '''
+
+def make_draggable(widget):
+    widget.bind("<Button-1>", on_drag_start)
+    widget.bind("<B1-Motion>", on_drag_motion)
+
+def on_drag_start(event):
+    widget = event.widget
+    widget._drag_start_x = event.x
+    widget._drag_start_y = event.y
+
+def on_drag_motion(event):
+    widget = event.widget
+    x = widget.winfo_x() - widget._drag_start_x + event.x
+    y = widget.winfo_y() - widget._drag_start_y + event.y
+    widget.place(x=x, y=y)
+
+
+
+class DragDropMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        make_draggable(self)
+
+class DnDFrame(DragDropMixin, tk.Frame):
+    pass
+
+main = tk.Tk()
+
+frame = DnDFrame(main, bd=4, bg="grey")
+frame.place(x=10, y=10)
+
+notes = tk.Text(frame)
+notes.pack()
+
+''' #starts networked threads '''
+
 
 t = NetThread()
 t.start()
