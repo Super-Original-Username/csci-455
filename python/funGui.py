95cf7d02a9aef5e3ec47bb2181a933f95a8d7fa9
Super-Original-Username <aswordlessninja@gmail.com>
2020-03-06 10:43:52 -0700

    gui now has dragndrop goodies

--- python/funGui.py
+++ python/funGui.py
@@ -19,6 +19,8 @@ def on_drag_motion(event):
     widget.place(x=x, y=y)
 
 
+def check_exec_order():
+
 
 class DragDropMixin:
     def __init__(self, *args, **kwargs):
@@ -30,13 +32,58 @@ class DnDFrame(DragDropMixin, tk.Frame):
     pass
 
 
-main = tk.Tk()
+main = tk.Tk(width=500,height=500)
 
-frame = DnDFrame(main, bg="white",height=10,width=10)
-frame.place(x=10, y=10)
+s = tk.Style()
 
+s.configure()
+
+h_rot_frame = DnDFrame(main, bg="white",cursor="dot",bd=4)
+h_rot_frame.place(x=10, y=10)
+
+hrlabel = tk.Label(h_rot_frame,text="head tilt")
+hrlabel.pack()
+
+h_turn_frame = DnDFrame(main, bg="white",cursor="dot",bd=4)
+h_turn_frame.place(x=10, y=40)
+
+htlabel = tk.Label(h_turn_frame,text="rotate head")
+htlabel.pack()
+
+b_right_frame = DnDFrame(main, bg="white",cursor="dot",bd=4)
+b_right_frame.place(x=10, y=70)
+
+brlabel = tk.Label(b_right_frame,text="body right")
+brlabel.pack()
+
+b_left_frame = DnDFrame(main, bg="white",cursor="dot",bd=4)
+b_left_frame.place(x=10, y=100)
+
+bllabel = tk.Label(b_left_frame,text="body left")
+bllabel.pack()
+
+arm_frame = DnDFrame(main, bg="white",cursor="dot",bd=4)
+arm_frame.place(x=10, y=130)
+
+armlabel = tk.Label(arm_frame,text="move arm")
+armlabel.pack()
+
+fw_mv_frame = DnDFrame(main, bg="white",cursor="dot",bd=4)
+fw_mv_frame.place(x=10, y=160)
+
+fwlabel = tk.Label(fw_mv_frame,text="move forward")
+fwlabel.pack()
+
+bw_mv_frame = DnDFrame(main, bg="white",cursor="dot",bd=4)
+bw_mv_frame.place(x=10, y=190)
+
+bwlabel = tk.Label(bw_mv_frame,text="move backward")
+bwlabel.pack()
+
+'''
 notes = tk.Text(frame)
 notes.pack()
+'''
 
 #label = DnDlabel
 
