14d3124e010f18fd621544c3fa03365ab5e3ec26
SAO <sao@orser.org>
2020-03-12 13:45:43 -0600

    Update funnerGUI.py

--- python/funnerGUI.py
+++ python/funnerGUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk 
 import queue
+from Maestro import Controller
 import time
 # from pololu import maestro as ms
 
@@ -7,34 +8,67 @@ action_queue = queue.Queue()
 queue_string = 'Current Queue: '
 action_string = 'Current Action: '
 
+motors = 1
+full_turn = 0
+body = 2
+h_turn = 3
+h_tilt = 4
+
+default_speed = 10
+default_accel = 10
+default_pos = 6000
+
+class ButtonController():
+    def __init__(self,win):
+        self.root = win
+        self.tango = Controller()
+        self.body = default_pos
+        self.headturn = default_pos
+        self.headtilt = default_pos
+        self.motors = 0
+        self.turn = 0
+        self.tango.setAccel(motors, default_accel)
+        self.tango.setAccel(full_turn,default_accel)
+        for i in range(1,6):
+                self.tango.setTarget(i,default_pos)
+
+                
+    def t1(self):                               # Head Tilt
+        self.tango.setTarget(h_tilt, 7000)
+        self.tango.setTarget(h_tilt, 5000)
+        self.tango.setTarget(h_tilt, 6000)
 
 
-def t1():
-    print(1)
+    def t2():                               # Head Turn
+        self.tango.setTarget(h_turn, 7000)
+        self.tango.setTarget(h_turn, 5000)
+        self.tango.setTarget(h_turn, 6000)
 
 
-def t2():
-    print(2)
+    def t3():                               # Torso Turn
+        self.tango.setTarget(body, 7000)
+        self.tango.setTarget(body, 5000)
+        self.tango.setTarget(body, 6000)
 
 
-def t3():
-    print(3)
+    def t4():                               # Forward (7000) for 1.5s
+        self.tango.setTarget(motors, 7000)
+        time.sleep(1.5)
+        self.tango.setTarget(motors, 6000)
 
 
-def t4():
-    print(4)
+    def t5():                               # Backwards(5000) for 1.5s
+        self.tango.setTarget(motors, 5000)
+        time.sleep(1.5)
+        self.tango.setTarget(motors, 6000)
 
 
-def t5():
-    print(5)
+    def t6():                               # Turn clockwise
+        print(6)
 
 
-def t6():
-    print(6)
-
-
-def t7():
-    print(7)
+    def t7():                               # Turn counter-clockwise
+        print(7)
 
 
 
@@ -63,6 +97,7 @@ move_dict = {'htilt':t1,'hrot':t2,'trot':t3,'fw':t4,'bw':t5,'cw':t6,'ccw':t7}
 
 
 win = tk.Tk()
+buttons = ButtonController(win)
 #win.attributes('-fullscreen',True) # this *should* just maximize the window. Otherwise uncomment the below line and adjust to fit
 win.geometry("1280x720")
 btn_box =tk.Frame(win)
@@ -90,7 +125,7 @@ ccw_btn = tk.Button(btn_box,text="Turn Counter-clockwise", command =lambda: line
 ccw_btn.grid(padx=25, pady=10, row=3, column=6) 
 
 go_btn = tk.Button(btn_box,text="Run Program", command = go)
-go_btn.grid(padx=25, pady=10, row=5, column=3) 
+go_btn.grid(padx=25, pady=10, row=5, column=3) s
 
 #close_btn = tk.Button(btn)
 
