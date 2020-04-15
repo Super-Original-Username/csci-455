545ba8bff9c4de3a730c2930dc493445b8ed69d0
Super-Original-Username <aswordlessninja@gmail.com>
2020-04-15 17:05:44 -0600

    back to other computer

--- python/collisiontester.py
+++ python/collisiontester.py
@@ -1,7 +1,12 @@
 """collisiontester controller."""
 
-"""This program has a maybe slightly different than expected. It works by sending the robot along a path 
-searching for dead ends with a turn priority of left, right, forward. It uses a stack to keep track of the current options for """
+"""
+his program has a maybe slightly different than expected. It works by sending the robot along a path 
+searching for dead ends with a turn priority of left, right, forward. It uses a stack to keep track of 
+the current options for a path, and ends with a valid path based off of the first turn that doesn't lead
+to a dead end. It does this twice, trying a different path in its second run. Of the two runs, the path 
+with the least number of turns is picked as the final path for run 3.
+"""
 
 
 # You may need to import some classes of the controller module. Ex:
@@ -13,24 +18,38 @@ import math
 # create the Robot instance.
 robot = Robot()
 
-max_speed = math.tau
+max_speed = math.tau # I chose to use tau instead of pi because I'm a rebel and like unnecessary challenges
+base_speed = max_speed/2
 
+
+def get_file_length(file):
+    with open(file) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 
 first_run = ''
 second_run = ''
-this_run = 0
+this_run = 1
 
 try:
     first_run = open('run1.txt')
-    this_run += 1
-except Exception:
+    print("Data from run one found")
+    this_run = 2
+    try:
+        second_run = open('run2.txt')
+        print("Data from run two found, calculating faster route")
+        this_run = 3
+    except:
+        pass
+except: # we don't actually care if a file isn't found, since that probably just means this is a new run
     pass
 
-try:
-    second_run = open('run2.txt')
+
 
 # get the time step of the current world.
 timestep = 64
+
 
 path_stack = LifoQueue()
 ps=[]
@@ -83,13 +102,18 @@ def make_your_move(in_dir, in_rotation):
     option_stack = []
     found_trophy = False
     if in_dir == 'l':
-        wb_motor_set_position(right_motor,-1.5708)
-        wb_motor_set_position(left_motor,-1.5708)
+        right_motor.setPosition(-max_speed/4)
+        left_motor.setPosition(-max_speed/4)
     elif in_dir=='r':
-        wb_motor_set_position(right_motor,1.5708)
-        wb_motor_set_position(left_motor,1.5708)
+        right_motor.setPosition(max_speed/4)
+        left_motor.setPosition(max_speed/4)
 
-    while check_walls.front == True:
+    # initial check for available turns
+    sensor_status = check_walls()
+    turn_available = sensor_status[0] or sensor_status[2]
+
+
+    while check_walls().front == True:
         if option_stack:
             found_trophy = make_your_move(option_stack.pop())
         if found_trophy == True:
@@ -122,3 +146,8 @@ while robot.step(timestep) != -1:
     pass
 
 # Enter here exit cleanup code.
+
+
+if this_run == 3: # clears out run files so we can relearn the position of the gem
+    remove('run1.txt')
+    remove('run2.txt')
